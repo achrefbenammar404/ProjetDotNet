84e4f698c7e6924fc12f1112a83fa491c4e8df73
Ret2Hell <mohamedyassine.taieb@insat.ucar.tn>
2025-01-28 09:18:06 +0100

    add authentication views and styles

---
+++ Controllers/Auth.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetDotNet.Controllers;
+
+public class Auth : Controller
+{
+    public IActionResult Login() => View();
+    public IActionResult Register() => View();
+}
